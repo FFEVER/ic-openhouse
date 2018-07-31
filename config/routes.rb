29a03b35d555c3654af92608a19188409404258f
Nattaphol <rootncdev2015@gmail.com>
2018-07-31 11:04:59 +0700

    Add webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: { format: :json } do
+    resources :quotes, only: [:show]
+  end
+  get "/*path", to: "pages#index", as: :app
+
+  root "pages#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
