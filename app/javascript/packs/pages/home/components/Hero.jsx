79a5840340b2200143d637eaac758a91fb3bb3e2
Nattaphol <rootncdev2015@gmail.com>
2018-08-01 16:57:04 +0700

    Add hero image bg and styles

---
+++ app/javascript/packs/pages/home/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Hero extends Component {
+  render() {
+    return (
+      <section className="Hero"></section>
+    );
+  }
+}
+
+export default Hero;
--- app/javascript/packs/pages/home/index.jsx
+++ app/javascript/packs/pages/home/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Hero />
         Hello
       </div>
     );
