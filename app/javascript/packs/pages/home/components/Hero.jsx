b6278f28b85f767174b8f983c32ae4a116f5b4f7
Nattaphol <rootncdev2015@gmail.com>
2018-08-02 16:03:07 +0700

    Add text logo

--- app/javascript/packs/pages/home/components/Hero.jsx
+++ app/javascript/packs/pages/home/components/Hero.jsx
@@ -4,10 +4,36 @@ import ParticlesSpace from "../../../plugins/particlesInSpace";
 class Hero extends Component {
   render() {
     return (
-      <section classNames="Hero">
-        <ParticlesSpace className="ParticlesSpace"/>
+      <section className="Hero">
+        <ParticlesSpace className="ParticlesSpace" />
+        <div className="logo-row">
+          <div className="logo-col">
+            <img
+              className="rabbit"
+              src={require("./images/ic-logo-rabbit.png")}
+            />
+          </div>
+          <div className="logo-col">
+            <img
+              className="text"
+              src={require("./images/ic-logo-text-1.png")}
+            />
+            <img
+              className="text"
+              src={require("./images/ic-logo-text-2.png")}
+            />
+            <img
+              className="text"
+              src={require("./images/ic-logo-text-3.png")}
+            />
+            <img
+              className="text"
+              src={require("./images/ic-logo-text-4.png")}
+            />
+          </div>
+        </div>
       </section>
-        );
+    );
   }
 }
 
