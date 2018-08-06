0209669132e53575a0f0fd9e6b84cc7a8fcd1033
Nattaphol <rootncdev2015@gmail.com>
2018-08-06 14:03:01 +0700

    Add parallax

--- app/javascript/packs/pages/home/index.jsx
+++ app/javascript/packs/pages/home/index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { ParallaxProvider } from "react-scroll-parallax";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
@@ -11,6 +13,14 @@ class Home extends Component {
         <div className="Hero Hero--yellow"></div>
         <div className="Hero Hero--green"></div>
       </div>
+      <ParallaxProvider>
+        <div>
+          <Navbar />
+          <Hero />
+          <div className="Hero Hero--yellow" />
+          <div className="Hero Hero--green" />
+        </div>
+      </ParallaxProvider>
     );
   }
 }
