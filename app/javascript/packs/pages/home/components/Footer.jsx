69fb611943ea4679959650ded23d143844845c6d
anyafleur <anyay.ks@gmail.com>
2018-08-06 13:42:40 +0700

    Add footer

---
+++ app/javascript/packs/pages/home/components/Footer.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import $ from "jquery";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="Footer">
+        <div className="container">
+          <div className="Footer__ic">
+            <h1>
+              <img src={require("./images/ic-icon-full.png")} />
+            </h1>
+            <p>King Mongkut's Institute of Technology</p>
+            <p>Ladkrabang International College</p>
+            <p>Bangkok, Thailand 10520</p>
+          </div>
+
+          <div className="Footer__icon">
+            <ul>
+              <li className="var_nav">
+                <div className="link_bg link_bg--fb" />
+                <div className="link_title">
+                  <a href="https://www.facebook.com/interkmitl/">
+                    <span>SE KMITL</span>
+                  </a>
+                  <div className="icon">
+                    <i className="fab fa-facebook-f" />
+                  </div>
+                </div>
+              </li>
+              <li className="var_nav">
+                <div className="link_bg link_bg--li" />
+                <div className="link_title">
+                  <a href="#">
+                    <span>line</span>
+                  </a>
+                  <div className="icon">
+                    <i className="fab fa-line" />
+                  </div>
+                </div>
+              </li>
+              <li className="var_nav">
+                <div className="link_bg link_bg--tw" />
+                <div className="link_title">
+                  <a href="#">
+                    <span>twitter</span>
+                  </a>
+                  <div className="icon">
+                    <i className="fab fa-twitter" />
+                  </div>
+                </div>
+              </li>
+              <li className="var_nav">
+                <div className="link_bg link_bg--phone" />
+                <div className="link_title">
+                  <a href="#">
+                    <span>088 888 8888</span>
+                  </a>
+                  <div className="icon">
+                    <i className="fas fa-phone" />
+                  </div>
+                </div>
+              </li>
+              <li className="var_nav">
+                <div className="link_bg link_bg--email" />
+                <div className="link_title">
+                  <a href="#">
+                    <span>mail@mail.com</span>
+                  </a>
+                  <div className="icon">
+                    <i className="fas fa-envelope" />
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <ul className="Footer__social">
+            <li>
+              <a href="https://www.facebook.com/interkmitl/">
+                <i className="fab fa-facebook-f" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fab fa-line" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fab fa-twitter" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fas fa-phone" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fas fa-envelope" />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="Footer__copyright">
+          <div className="container">
+            <p>&copy; IC KMITL Open House 2018. All Rights Reserved</p>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
--- app/javascript/packs/pages/home/index.jsx
+++ app/javascript/packs/pages/home/index.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <div className="Hero Hero--yellow"></div>
-        <div className="Hero Hero--green"></div>
+        <div className="Hero Hero--green" />
+        <Footer />
       </div>
     );
   }
