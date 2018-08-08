a5dced59f0bda1c36a5ac63d07af3a4b6e65966b
Nattaphol <rootncdev2015@gmail.com>
2018-08-08 10:05:04 +0700

    Add moveto

--- app/javascript/packs/pages/home/components/Navbar.jsx
+++ app/javascript/packs/pages/home/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import $ from "jquery";
+import MoveTo from "moveto";
 
 class Navbar extends Component {
   componentWillMount() {
     this.navChangeColor();
+  }
+
+  onLinkClicked(event) {
+    const moveTo = new MoveTo();
+    const target = document.getElementById(event.currentTarget.dataset.id);
+    moveTo.move(target);
   }
 
   navChangeColor() {
@@ -11,10 +18,15 @@ class Navbar extends Component {
       $(document).scroll(function() {
         var $nav = $(".navbar.fixed-top");
         var $hero = $(".Hero.Hero--yellow");
-        $nav.toggleClass(
-          "scrolled",
-          $(this).scrollTop() > $hero.height() - $nav.height()
-        );
+        var $brand = $("#navbar-brand");
+
+        if ($(this).scrollTop() > $hero.height() - $nav.height()) {
+          $brand.removeClass("hidden");
+          $nav.addClass("scrolled");
+        } else {
+          $brand.addClass("hidden");
+          $nav.removeClass("scrolled");
+        }
       });
     });
   }
@@ -22,7 +34,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-md navbar-transparent fixed-top">
-        <a className="navbar-brand" href="#">
+        <a id="navbar-brand" className="navbar-brand hidden" href="#">
           <img src={require("./images/ic-logo-rabbit.png")} />
         </a>
         <button
@@ -36,30 +48,30 @@ class Navbar extends Component {
         >
           <span className="navbar-toggler-icon" />
         </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#Info">
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" data-id="description" onClick={this.onLinkClicked}>
                 DESCRIPTION
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" data-id="activities" onClick={this.onLinkClicked}>
                 ACTIVITIES
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" data-id="schedule" onClick={this.onLinkClicked}>
                 SCHEDULE
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" data-id="courses" onClick={this.onLinkClicked}>
                 COURSES
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" data-id="map" onClick={this.onLinkClicked}>
                 MAP
               </a>
             </li>
