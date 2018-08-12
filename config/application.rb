2c1a334447973ac99011874271bc5b8d904f8b4c
Nattaphol <rootncdev2015@gmail.com>
2018-08-12 15:28:35 +0700

    try changing webpack config

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module IcOpenhouse
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.precompile += %w(index.js)
   end
 end
