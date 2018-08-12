40840e6ceb37e7aff35679a7e6f3795a1032343c
Nattaphol <rootncdev2015@gmail.com>
2018-08-12 11:43:53 +0700

    config bootstrap css

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import App from "./App";
 import 'bootstrap/dist/js/bootstrap';
+import "./bootstrap.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
